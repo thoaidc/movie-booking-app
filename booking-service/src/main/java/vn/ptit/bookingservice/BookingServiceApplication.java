3e1d1c7942d751359d53eb86a28b640914c77188
thoaidc <dcthoai1023@gmail.com>
2025-07-24 21:06:52 +0700

    init base project

---
+++ booking-service/src/main/java/vn/ptit/bookingservice/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.ptit.bookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
+
+}
